c38b6a695e0bc7d8f8a28d3f158ecdb26f8002ce
Celso Gonçalves <celsottgoncalves@gmail.com>
2026-02-13 06:52:04 -0300

    Add logo and implement Aside component with styling
    - Introduced a new logo image for the application.
    - Created global styles for the body background.
    - Added an Aside component that includes the logo and applies specific styles.
    - Updated the layout to incorporate the Aside component alongside the main content.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,6 @@
+import "./globals.css";
+import { Aside } from "@/components/Aside";
+
 export const metadata = {
   title: "Code Connect",
   description: "Code Connect is a platform for connecting with other developers",
@@ -6,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <Aside />
+        {children}
+        </body>
     </html>
   );
 }
